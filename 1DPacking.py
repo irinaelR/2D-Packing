124e7020e24fed37313dad3d0661acd41ccc3e77
El <irina.elina.r@gmail.com>
2024-07-04 20:32:28 +0300

    Update 1DPacking.py
    finished worst fit

--- 1DPacking.py
+++ 1DPacking.py
@@ -39,6 +39,7 @@ def first_fit(shapes, B):
     return bacs
 
 import sys
+
 def best_fit(shapes, B):
     bacs = [Bac([], B)] # we begin with one empty bac of the size B
 
@@ -67,7 +68,33 @@ def best_fit(shapes, B):
 
     return bacs
             
+def worst_fit(shapes, B):
+    bacs = [Bac([], B)] # we begin with one empty bac of the size B
 
+    for shape in shapes:
+
+        if shape.size <= B:
+
+            max_space_left = -sys.maxsize - 1
+            bac_index = -1
+
+            for index, bac in enumerate(bacs):
+
+                space_left = bac.get_remaining_surface() - shape.size
+
+                if max_space_left < space_left and space_left >= 0:
+                    # minimizing the space left after insertion AND making sure the shape can actually fit
+                    bac_index = index
+                    max_space_left = space_left
+            
+            if bac_index >= 0:
+                # a valid bac was found to insert the shape into
+                bacs[bac_index].shapes.append(shape)
+            else:
+                # we add a new bac to accomodate
+                bacs.append(Bac([shape], B))
+
+    return bacs
 
 B = 5
 shapes = [Shape(2), Shape(4), Shape(1), Shape(4), Shape(3)]
@@ -84,3 +111,8 @@ filled_bacs = best_fit(shapes, B)
 for index, bac in enumerate(filled_bacs):
     print(f"Bac index: {index}, Shapes: {[shape.size for shape in bac.shapes]}")
 
+print("worst Fit 1D")
+filled_bacs = worst_fit(shapes, B)
+    # i want to print the bac's index and its shapes array (bac.shapes)
+for index, bac in enumerate(filled_bacs):
+    print(f"Bac index: {index}, Shapes: {[shape.size for shape in bac.shapes]}")
